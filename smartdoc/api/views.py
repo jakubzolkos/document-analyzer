8eebfd890ea3f48a48c1fd587b2009dc42ba8cd2
jakubzolkos <zolkos@bu.edu>
2023-03-07 13:26:49 -0500

    API views prototypes created

--- smartdoc/api/views.py
+++ smartdoc/api/views.py
@@ -1,3 +1,86 @@
 from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
-# Create your views here.
+
+class FileUpload(APIView):
+    """
+    Enables file uploading for the user
+    """
+    def post(self, request, *args, **kwargs):
+        return HttpResponse("This is a POST request.")
+
+
+class ParagraphByKeyword(APIView):
+    """ 
+    Finds all paragraphs that contain a given keyword
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class ParagraphBySentiment(APIView):
+    """ 
+    Finds all paragraphs with a given sentiment
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class DefinitionByKeyword(APIView):
+    """
+    Returns the definition of the input keyword
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class SummaryByDocID(APIView):
+    """ 
+    Returns the summary of a document with a given ID
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class TopicByDocID(APIView):
+    """ 
+    Returns the topic of a document with a given ID
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class Institutions(APIView):
+    """
+    Return the list of all institutions in the documents
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class Locations(APIView):
+    """
+    Return the list of all locations in the documents
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")
+
+
+class Addresses(APIView):
+    """
+    Return the list of all addresses in the documents
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")    
+
+
+class Names(APIView):
+    """
+    Return the list of all names in the documents
+    """
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("This is a GET request.")    
